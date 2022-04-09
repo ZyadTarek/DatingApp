ca569fb8fbec349b73568b3a392616244850ad2a
Zyad Tarek <zelnegely@ejada.com>
2022-04-09 14:14:05 +0300

    End of section 13

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using API.DTOs;
 using API.Entities;
 using API.Extensions;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
@@ -30,9 +31,18 @@ namespace API.Controllers
         }
         [HttpGet]
         [AllowAnonymous]
-        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
-        {
-            return Ok(await _userRepository.GetMembersAsync());
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers([FromQuery] UserParams userParams)
+        {   
+            var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+            userParams.CurrentUsername = user.UserName;
+
+            if(string.IsNullOrEmpty(userParams.Gender))
+                userParams.Gender = (user.Gender == "male") ? "female" : "male";
+
+            var users = await _userRepository.GetMembersAsync(userParams);  
+            Response.AddPaginationHeader(users.CurrentPage, users.PageSize,
+             users.TotalCount, users.TotalPages);
+            return Ok(users);
         }
         [Authorize]
         [HttpGet("{username}", Name = "GetUser")]
