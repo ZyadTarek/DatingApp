7b0edc29c9aac174fe8e1a2316c9ccfa25db1b6f
Zyad Tarek <zelnegely@ejada.com>
2022-04-13 10:27:46 +0300

    End of section 14

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -76,12 +76,25 @@ export class MembersService {
       })
     );
   }
+
   setMainPhoto(photoId: number){
     return this.http.put(this.baseUrl + 'users/set-main-photo/' + photoId, {});
   }
+
   deletePhoto(photoId: number){
     return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
   }
+
+  addLike(username: string){
+    return this.http.post(`${this.baseUrl}likes/${username}`,{});
+  }
+
+  getLikes(predicate: string, pageNumber, pageSize){
+    let params = this.getPaginationHeaders(pageNumber,pageSize);
+    params = params.append('predicate',predicate);
+    return this.getPaginatedResult<Partial<Member[]>>(this.baseUrl + 'likes',params);
+  }
+
   private getPaginatedResult<T>(url, params: HttpParams) {
     const paginatedResult: PaginatedResult<T> = new PaginatedResult<T>();
 
